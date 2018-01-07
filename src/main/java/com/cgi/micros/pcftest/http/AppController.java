95e9f2842c70f19432861c734f95e3b353c0d19c
Daniel Beyer <danielbeyer@cgi.com>
2018-01-07 17:54:20 +0100

    Added a User and mods.

--- src/main/java/com/cgi/micros/pcftest/http/AppController.java
+++ src/main/java/com/cgi/micros/pcftest/http/AppController.java
@@ -1,13 +1,18 @@
 package com.cgi.micros.pcftest.http;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
+	
+	HashMap mapUsers = new HashMap();
 
 	@Autowired
 	public AppController() {
@@ -25,12 +30,19 @@ public class AppController {
 
 	}
 	
-	@RequestMapping("/user")
-    public User greeting(@RequestParam(value="vorName", defaultValue="World") String vorName,
-    							@RequestParam(value="nachName", defaultValue="World") String nachName,
-    								@RequestParam(value="telfonnummer", defaultValue="World") String telefonnummer,
-    									@RequestParam(value="personalnummer", defaultValue="World") String personalnummer) {
-        return new User(vorName, nachName, telefonnummer, personalnummer);
+	@RequestMapping(value="/user", method = RequestMethod.POST)
+    public HashMap<UUID, User> administrateUser(@RequestParam(value="vorName", defaultValue="Harry") String vorName,
+    							@RequestParam(value="nachName", defaultValue="Hirsch") String nachName,
+    								@RequestParam(value="telfonnummer", defaultValue="00496099921954") String telefonnummer,
+    									@RequestParam(value="personalnummer", defaultValue="test@pcf.io") String personalnummer) {
+		
+       User user = new User(vorName, nachName, telefonnummer, personalnummer);
+       mapUsers.put(UUID.randomUUID(), user); 
+       return mapUsers;
     }
 	
+	@RequestMapping(value="/user", method= RequestMethod.GET)
+	public HashMap<UUID, User> getUsers() {
+		return mapUsers;
+	}
 }
